94cfddb82deb146d4213507666729bea6116b9ff
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-10-08 19:22:03 +0800

    added player health

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -11,8 +11,8 @@ func die():
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		body.die()
-		die() 
+		body.take_damage(1)
+		take_damage(1) 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
