7d4106e6ffe8d5b92c8d1cc5b1d41d54d10d1da4
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-10-08 16:44:15 +0800

    hold spacebar to continuously shoot

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,14 +3,21 @@ extends CharacterBody2D
 signal bullet_shot(bullet_scene, location)
 
 @export var speed = 300
+@export var rate_of_fire := 0.25
 
 @onready var muzzle = $Muzzle
 
 var bullet_scene = preload("res://scenes/bullet.tscn")
 
+var shoot_cd := false
+
 func _process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	if Input.is_action_pressed("shoot"):
+		if !shoot_cd:
+			shoot_cd = true
+			shoot()
+			await get_tree().create_timer(rate_of_fire).timeout
+			shoot_cd = false
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("left",
