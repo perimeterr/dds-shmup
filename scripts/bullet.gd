736eed66f01521953f1ca5f3e51ca6af511d3bea
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-10-09 01:47:18 +0800

    added bullet explosions

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,9 +3,13 @@ extends Area2D
 @export var speed = 600
 @export var damage = 1
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	animated_sprite.animation = "default"
+
 func _physics_process(delta):
 	global_position.x += speed * delta
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
@@ -14,4 +18,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area is Enemy:
 		area.take_damage(damage)
+		animated_sprite.animation = "explosion"
+		var tween = create_tween()
+		tween.tween_property(animated_sprite, "scale", Vector2(3,3), 0.05)
+		await tween.finished
 		queue_free()
--- scripts/enemy_bullet.gd
+++ scripts/enemy_bullet.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed = 600
 @export var damage = 1
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	global_position.x += -speed * delta
 
@@ -12,4 +14,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if body is Player:
 		body.take_damage(damage)
+		animated_sprite.animation = "explosion"
+		var tween = create_tween()
+		tween.tween_property(animated_sprite, "scale", Vector2(3,3), 0.05)
+		await tween.finished
 		queue_free()
