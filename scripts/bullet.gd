e653c9b3cb08bb29d3f28d4ad639e28d74c2561c
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-10-08 19:01:59 +0800

    added enemy hp

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var speed = 600
+@export var damage = 1
 
 func _physics_process(delta):
 	global_position.x += speed * delta
@@ -12,5 +13,5 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is Enemy:
-		area.die()
+		area.take_damage(damage)
 		queue_free()
